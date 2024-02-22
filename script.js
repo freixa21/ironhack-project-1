744feba61d54acfd4e12d5bcd45488d294fb3bbc
Max Freixa <freixa.sureda.max@alumnat.copernic.cat>
2024-02-22 20:45:28 +0100

    api fix

--- script.js
+++ script.js
@@ -2,9 +2,15 @@
         const menuToggle = document.getElementById('menu-toggle');
         const navList = document.querySelector('nav ul');
         const conctactButton = document.querySelector('#contact-button');
+        const menuIconOne = document.getElementById('menu-icon-1');
+        const menuIconTwo = document.getElementById('menu-icon-2');
 
         menuToggle.addEventListener('click', function() {
             navList.classList.toggle('active');
             conctactButton.classList.toggle('active');
+
+            menuIconOne.classList.toggle('active');
+            menuIconTwo.classList.toggle('active');
+
         });
     });
