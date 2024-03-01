e4fbf1a15e6a2e2f4456352b19a2ce891ef7ff05
Max Freixa <freixa.sureda.max@alumnat.copernic.cat>
2024-03-01 20:36:04 +0100

    fix contact link mobile

--- script.js
+++ script.js
@@ -4,10 +4,12 @@
         const conctactButton = document.querySelector('#contact-button');
         const menuIconOne = document.getElementById('menu-icon-1');
         const menuIconTwo = document.getElementById('menu-icon-2');
+        const contactLink = document.getElementById('contact-mobile');
 
         menuToggle.addEventListener('click', function() {
             navList.classList.toggle('active');
             conctactButton.classList.toggle('active');
+            contactLink.classList.toggle('hide');
 
             menuIconOne.classList.toggle('active');
             menuIconTwo.classList.toggle('active');
